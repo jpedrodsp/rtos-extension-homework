2bb21ac5aee02bda4dda57e279d76d7130cf7dc3
João Pedro Patrocinio <jpedro9966@gmail.com>
2023-09-18 21:05:27 -0300

    Add display task boilerplate

--- main/main.c
+++ main/main.c
@@ -10,29 +10,38 @@
 
 #include "esp_log.h"
 
-void init_hw(void)
+typedef struct inithw_s {
+    ssd1306_handle_t hndDisplayDevice;
+} inithw_t;
+
+inithw_t init_hw(void)
 {
+    inithw_t inithw;
     // Configure PINs/Connections for DHT22
     {
         // Not needed if using DHT Library
     }
     // Configure PINs/Connections for Display
     {
-
+        inithw.hndDisplayDevice = setup_display();
     }
     // Configure PINs/Connections for Actuator
     {
 
     }
+    return inithw;
 }
 
 void app_main(void)
 {
-    init_hw();
+    inithw_t config = init_hw();
     // Create Umidity Sensor Task
     xTaskCreate(task_umiditysensor, TASK_UMIDITYSENSOR_NAME, TASK_UMIDITYSENSOR_STACKSIZE, NULL, 1, NULL);
     // Create Display Task
-    xTaskCreate(task_display, TASK_DISPLAY_NAME, TASK_DISPLAY_STACKSIZE, NULL, 1, NULL);
+    display_pvparameters_t display_pvparameters = {
+        .dev = config.hndDisplayDevice
+    };
+    xTaskCreate(task_display, TASK_DISPLAY_NAME, TASK_DISPLAY_STACKSIZE, &display_pvparameters, 1, NULL);
     // Create Umidity Actuator Task
     xTaskCreate(task_umidityactuator, TASK_UMIDITYACTUATOR_NAME, TASK_UMIDITYACTUATOR_STACKSIZE, NULL, 1, NULL);
 }
--- main/tasks/display.h
+++ main/tasks/display.h
@@ -1,14 +1,33 @@
 #include "defaults.h"
 
 #include "freertos/task.h"
+#include "driver/gpio.h"
+
+#include "ssd1306.h"
 
 #include "esp_log.h"
 
 #define TASK_DISPLAY_NAME "display"
 #define TASK_DISPLAY_STACKSIZE 2048
+#define TASK_DISPLAY_I2CPORT I2C_NUM_0 // defined in gpio.h
+
+typedef struct display_pvparameters_s {
+    ssd1306_handle_t dev;
+} display_pvparameters_t;
+
+ssd1306_handle_t setup_display(void)
+{
+    i2c_port_t i2cport = TASK_DISPLAY_I2CPORT;
+    ssd1306_handle_t dev = ssd1306_create(i2cport, SSD1306_I2C_ADDRESS);
+    ssd1306_init(dev);
+    ssd1306_clear_screen(dev, false);
+    ssd1306_refresh_gram(dev);
+    return dev;
+}
 
 void task_display(void *pvParameters)
 {
+    ssd1306_handle_t dev = ((display_pvparameters_t *) pvParameters)->dev;
     ESP_LOGI(TASK_DISPLAY_NAME, "Task started");
     while(1)
     {
