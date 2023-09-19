1da0c02a85f349bb3a5755988b8638aebbb7d482
DevUnusual <carlos.meneses.g.s@gmail.com>
2023-09-19 16:56:44 -0300

    Setup display pos; Temp static value array umid Static value array problems: xqueu handle not acess

--- main/main.c
+++ main/main.c
@@ -20,19 +20,18 @@ typedef struct inithw_s
 inithw_t init_hw(void)
 {
     inithw_t inithw = {
-        .hndDisplayDevice = NULL
-    };
+        .hndDisplayDevice = NULL};
     // Configure PINs/Connections for DHT22
     {
         // Not needed if using DHT Library
     }
     // Configure PINs/Connections for Display
     {
-        #ifdef TASK_DISPLAY_ENABLE
-        #if TASK_DISPLAY_ENABLE == 1
+#ifdef TASK_DISPLAY_ENABLE
+#if TASK_DISPLAY_ENABLE == 1
         inithw.hndDisplayDevice = setup_display();
-        #endif
-        #endif
+#endif
+#endif
     }
     // Configure PINs/Connections for Actuator
     {
@@ -45,46 +44,45 @@ void app_main(void)
     inithw_t config = init_hw();
     QueueHandle_t hndUmiditySensorQueue = xQueueCreate(2, sizeof(umidityqueue_data_t));
     xQueueReset(hndUmiditySensorQueue);
-    if (hndUmiditySensorQueue == NULL) {
+    if (hndUmiditySensorQueue == NULL)
+    {
         ESP_LOGE("main", "Error creating queue for Umidity Sensor");
     }
     printf("got address for queue [1]: %p\n", hndUmiditySensorQueue);
     printf("got address for queue [2]: %p\n", &hndUmiditySensorQueue);
 
-    // Create Umidity Sensor Task
-    #ifdef TASK_UMIDITYSENSOR_ENABLE
-    #if TASK_UMIDITYSENSOR_ENABLE == 1
+// Create Umidity Sensor Task
+#ifdef TASK_UMIDITYSENSOR_ENABLE
+#if TASK_UMIDITYSENSOR_ENABLE == 1
     // Create queue to receive data from Umidity Sensor
     umiditysensor_pvparameters_t umiditysensor_pvparameters = {
-        .hndUmiditySensorQueue = hndUmiditySensorQueue
-    };
+        .hndUmiditySensorQueue = hndUmiditySensorQueue};
     TaskHandle_t hndUmiditySensorTask;
     xTaskCreate(task_umiditysensor, TASK_UMIDITYSENSOR_NAME, TASK_UMIDITYSENSOR_STACKSIZE, &umiditysensor_pvparameters, 1, &hndUmiditySensorTask);
-    #endif
-    #endif
-    
-    // Create Display Task
-    #ifdef TASK_DISPLAY_ENABLE
-    #if TASK_DISPLAY_ENABLE == 1
+#endif
+#endif
+
+// Create Display Task
+#ifdef TASK_DISPLAY_ENABLE
+#if TASK_DISPLAY_ENABLE == 1
     TaskHandle_t hndDisplayTask;
     display_pvparameters_t display_pvparameters = {
         .dev = config.hndDisplayDevice};
     xTaskCreate(task_display, TASK_DISPLAY_NAME, TASK_DISPLAY_STACKSIZE, &display_pvparameters, 1, &hndDisplayTask);
-    #endif
-    #endif
+#endif
+#endif
 
-    // Create Umidity Actuator Task
-    #ifdef TASK_UMIDITYACTUATOR_ENABLE
-    #if TASK_UMIDITYACTUATOR_ENABLE == 1
+// Create Umidity Actuator Task
+#ifdef TASK_UMIDITYACTUATOR_ENABLE
+#if TASK_UMIDITYACTUATOR_ENABLE == 1
     umidityactuator_pvparameters_t umidityactuator_pvparameters = {
-        .hndUmiditySensorQueue = hndUmiditySensorQueue
-    };
+        .hndUmiditySensorQueue = hndUmiditySensorQueue};
     TaskHandle_t hndUmidityActuatorTask;
     xTaskCreate(task_umidityactuator, TASK_UMIDITYACTUATOR_NAME, TASK_UMIDITYACTUATOR_STACKSIZE, &umidityactuator_pvparameters, 1, &hndUmidityActuatorTask);
-    #endif
-    #endif
+#endif
+#endif
 
-    while(1)
+    while (1)
     {
         // This is done to prevent the main task from exiting, and thus deleting the variables declared in this function (scope)
         vTaskDelay(TASK_DEFAULTWAITTIME * 100);
--- main/tasks/display.h
+++ main/tasks/display.h
@@ -25,6 +25,9 @@ typedef struct display_pvparameters_s
 
 ssd1306_handle_t setup_display(void)
 {
+    char umid[3] = "30";
+    char temp[10] = "30 C";
+
     ssd1306_handle_t ssd1306_dev = NULL;
 
     i2c_config_t conf;
@@ -40,19 +43,28 @@ ssd1306_handle_t setup_display(void)
     i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);
 
     ssd1306_dev = ssd1306_create(I2C_MASTER_NUM, SSD1306_I2C_ADDRESS);
+
+    ssd1306_fill_rectangle(ssd1306_dev, 4, 3, 60, 21, 1);    // esquerda encima
+    ssd1306_fill_rectangle(ssd1306_dev, 71, 3, 125, 21, 1);  // Direita encima
+    ssd1306_fill_rectangle(ssd1306_dev, 4, 24, 60, 41, 1);   // esquerda meio
+    ssd1306_fill_rectangle(ssd1306_dev, 71, 24, 125, 41, 1); // Direita meio
+    ssd1306_fill_rectangle(ssd1306_dev, 4, 44, 125, 61, 1);  // baixo
+    ssd1306_draw_char(ssd1306_dev, 5, 25, 'T', 16, 0);
+    ssd1306_draw_string(ssd1306_dev, 15, 25, &temp, 16, 0);
+    ssd1306_draw_char(ssd1306_dev, 5, 4, 'C', 16, 0);
+    ssd1306_draw_char(ssd1306_dev, 72, 25, 'U', 16, 0);
+    ssd1306_draw_string(ssd1306_dev, 80, 25, &umid, 16, 0);
+    ssd1306_draw_char(ssd1306_dev, 96, 25, '%', 16, 0);
+    vTaskDelay(pdMS_TO_TICKS(1000));
     ssd1306_refresh_gram(ssd1306_dev);
     ssd1306_clear_screen(ssd1306_dev, 0x00);
-
-    char data_str[10] = {0};
-    sprintf(data_str, "C STR");
-    ssd1306_draw_string(ssd1306_dev, 70, 16, (const uint8_t *)data_str, 16, 1);
-    ssd1306_refresh_gram(ssd1306_dev);
     return ssd1306_dev;
 }
 
 void task_display(void *pvParameters)
 {
     ssd1306_handle_t dev = ((display_pvparameters_t *)pvParameters)->dev;
+    // QueueHandle_t hndDisplayQueue = ((display_pvparameters_t *)pvParameters)->hndDisplayQueue;
     ESP_LOGI(TASK_DISPLAY_NAME, "Task started");
     while (1)
     {
